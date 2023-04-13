0454add069e3744041bf36e6578e6c5bfb1f7924
Michael Putera Wardana <60507035+krobus00@users.noreply.github.com>
2023-04-13 19:35:15 +0700

    refactor: refactor object auth (#11)

--- internal/constant/permission.go
+++ internal/constant/permission.go
@@ -1,11 +1,20 @@
 package constant
 
+type ActionType string
+
 const (
-	PermissionFullAccess = "FULL_ACCESS"
-	PermissionAllowGuest = "GUEST_FULL_ACCESS"
+	ActionCreate ActionType = "CREATE"
+	ActionRead   ActionType = "READ"
+	ActionUpdate ActionType = "UPDATE"
+	ActionDelete ActionType = "DELETE"
+
+	PermissionFullAccess  = "FULL_ACCESS"
+	PermissionGuestAccess = "GUEST_ACCESS"
 
 	PermissionObjectAll         = "OBJECT_ALL"
 	PermissionObjectCreate      = "OBJECT_CREATE"
 	PermissionObjectRead        = "OBJECT_READ"
 	PermissionObjectReadPrivate = "OBJECT_READ_PRIVATE"
+	PermissionObjectDelete      = "OBJECT_DELETE"
+	PermissionObjectModifyOther = "OBJECT_MODIFY_OTHER"
 )
